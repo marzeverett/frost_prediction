43a11445d0fadd158b5d2edfe84f3a39ecc9f000
marzeverett <marzeverett@gmail.com>
2023-09-19 19:13:41 -0700

    Jornada non-sequence 6!

--- jornada_non_sequence_6.py
+++ jornada_non_sequence_6.py
@@ -120,9 +120,9 @@ params_dicts = {
 }
 
 #NAME - {phase_name}_{parameter_index}_{Run}
-phase_name = "Initial_5"
+phase_name = "Initial_6"
 key="frost"
-all_data=False
+all_data=True
 for param_dict_index in list(params_dicts.keys()):
     for run_index in list(runs.keys()):
         name = f'{phase_name}_{param_dict_index}_{run_index}'
