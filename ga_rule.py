62fa1f68c00bf180bdec264de456befb36e2e580
marzeverett <marzeverett@gmail.com>
2023-09-26 10:02:08 -0700

    typo

--- ga_rule.py
+++ ga_rule.py
@@ -278,7 +278,7 @@ class rule:
 
 
     def calc_overall_support(self, df):
-        if self.sequence:value
+        if self.sequence:
             self.calc_overall_support_sequence(df)
         else:
             self.calc_overall_support_non_sequence(df)
