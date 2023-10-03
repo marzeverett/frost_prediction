beeed51dc19e21e37ccc7ec4ca06c40e49c23974
marzeverett <marzeverett@gmail.com>
2023-10-03 12:30:05 -0700

    Changes to AIM

--- run_vital_sequences_AIM.py
+++ run_vital_sequences_AIM.py
@@ -3,7 +3,9 @@
 # import vital_sequence_E
 # import vital_sequence_F
 
-import vital_sequence_A
-import vital_sequence_B
-import vital_sequence_C
+# import vital_sequence_A
+# import vital_sequence_B
+# import vital_sequence_C
+
+import vital_non_sequence_F
 
